26decfba65fcf1e718ec0d35b44b94b5e3d05a08
nwbort <30829548+nwbort@users.noreply.github.com>
2025-12-02 14:34:56 +1100

    Update scrape.sh

--- scrape.sh
+++ scrape.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 ./download.sh 'https://api.bws.com.au/apis/ui/StoreLocator/Stores/bws?state=VIC&type=allstores&Max=999999'
+./download.sh 'https://api.bws.com.au/apis/ui/StoreLocator/Stores/bws?state=ACT&type=allstores&Max=999999'
+./download.sh 'https://api.bws.com.au/apis/ui/StoreLocator/Stores/bws?state=QLD&type=allstores&Max=999999'
+./download.sh 'https://api.bws.com.au/apis/ui/StoreLocator/Stores/bws?state=TAS&type=allstores&Max=999999'
+./download.sh 'https://api.bws.com.au/apis/ui/StoreLocator/Stores/bws?state=WA&type=allstores&Max=999999'
+./download.sh 'https://api.bws.com.au/apis/ui/StoreLocator/Stores/bws?state=NSW&type=allstores&Max=999999'
+./download.sh 'https://api.bws.com.au/apis/ui/StoreLocator/Stores/bws?state=SA&type=allstores&Max=999999'
+./download.sh 'https://api.bws.com.au/apis/ui/StoreLocator/Stores/bws?state=NT&type=allstores&Max=999999'
